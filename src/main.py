e9f665ef90497c4c4e1de5f45bc0f57b38be5cc0
00anp <alfonso.nunez29@gmail.com>
2024-04-03 20:38:01 -0600

    Main project structure

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from textnode import TextNode
+
+def main():
+    # Create a new TextNode object
+    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    
+    print(node)
+
+# Call the main function
+if __name__ == "__main__":
+    main()
